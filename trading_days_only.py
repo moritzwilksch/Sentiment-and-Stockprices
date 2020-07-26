076343d75a4ccc85ffb585ebe828a8b4a5cde23d
Moritz Wilksch <moritzwilksch@gmail.com>
2020-07-26 09:17:40 +0200

    cleanup

--- trading_days_only.py
+++ trading_days_only.py
@@ -13,31 +13,13 @@ from rich.console import Console
 
 c = Console()
 
-
-use_vader = True
-
-vader = pd.read_pickle('data/vader_out_clean.pickle')
-vader['datetime'] = vader.datetime.astype('datetime64')
-
-if use_vader:
-    print("====> Using VADER Data")
-    senti_df = vader.copy()
-else:
-    print("====> Using AFINN Data")
-    afinn = pd.read_pickle('data/afinn_alldata_2019')
-    afinn['sentiment'] = afinn.scores.copy()
-    afinn = afinn.drop(['scores'], axis=1)
-    afinn['datetime'] = afinn.date.astype('datetime64')
-    afinn = afinn.reset_index(drop=True)
-    afinn = pd.concat([afinn, vader[['likes', 'retweets', 'replies']]], axis=1)
-    senti_df = afinn.copy()
-
-
-
+print("====> Using VADER Data")
+vader_df = pd.read_pickle('data/vader_out_clean.pickle')
+vader_df['datetime'] = vader_df.datetime.astype('datetime64')
 
 
 print(pd.date_range(start='2019-01-01',
-                    end='2019-12-31').difference(senti_df.datetime.dt.date))
+                    end='2019-12-31').difference(vader_df.datetime.dt.date))
 
 
 raw_yahoo = yf.Ticker('TSLA').history(
@@ -47,31 +29,30 @@ raw_prices = raw_yahoo['Close']
 # %%
 # Backfill
 daily_return = raw_prices.pct_change().shift(-1)
-daily_return = daily_return.asfreq('D', method='ffill').fillna(0)
+# daily_return = daily_return.asfreq('D', method='ffill').fillna(0)
 
 # %%
-daily_sentiment: pd.Series = senti_df.groupby(senti_df.datetime.dt.date).agg(
-    lambda x: np.average(x, weights=senti_df.loc[x.index, "likes"]))['sentiment']
-pct_pos = senti_df.groupby(senti_df.datetime.dt.date)[
+daily_sentiment: pd.Series = vader_df.groupby(vader_df.datetime.dt.date).agg(
+    lambda x: np.average(x, weights=vader_df.loc[x.index, "likes"]))['sentiment']
+pct_pos = vader_df.groupby(vader_df.datetime.dt.date)[
     'sentiment'].agg(lambda s: sum(s > 0.05)/len(s))
-pct_neg = senti_df.groupby(senti_df.datetime.dt.date)[
+pct_neg = vader_df.groupby(vader_df.datetime.dt.date)[
     'sentiment'].agg(lambda s: sum(s < -0.05)/len(s))
-num_of_tweets = senti_df.groupby(senti_df.datetime.dt.date)['sentiment'].size()
+num_of_tweets = vader_df.groupby(vader_df.datetime.dt.date)['sentiment'].size()
 
-
-# %%
-df = pd.DataFrame({
-    'sentiment': daily_sentiment[1:364].reset_index(drop=True),
-    'pct_pos': pct_pos[1:364].reset_index(drop=True),
-    'pct_neg': pct_neg[1:364].reset_index(drop=True),
-    'num_tweets': num_of_tweets[1:364].reset_index(drop=True),
-    'volume': raw_yahoo['Volume'].asfreq('D', method='ffill').fillna(0).values,
-    'prev0': raw_prices.pct_change().shift(0).asfreq('D', method='ffill').reset_index(drop=True).fillna(0),
-    'prev1': raw_prices.pct_change().shift(1).asfreq('D', method='ffill').reset_index(drop=True).fillna(0),
-    'prev2': raw_prices.pct_change().shift(2).asfreq('D', method='ffill').reset_index(drop=True).fillna(0),
-    # 'prev4': raw_prices.pct_change().shift(3).asfreq('D', method='ffill').reset_index(drop=True).fillna(0),
-}
+#%%
+df = pd.merge(daily_return, daily_sentiment, left_index=True, right_index=True)
+df = (df
+    .assign(pct_pos=pct_pos[df.index])
+    .assign(pct_neg=pct_neg[df.index])
+    .assign(num_tweets=num_of_tweets[df.index])
+    .assign(volume=raw_yahoo['Volume'])
+    .assign(prev0=raw_prices.pct_change().shift(0).fillna(0))
+    .assign(prev1=raw_prices.pct_change().shift(1).fillna(0))
+    .assign(prev2=raw_prices.pct_change().shift(2).fillna(0))
+    .dropna()
 )
+
 c.print("[bold underline]Data Frame")
 df.info()
 
@@ -82,8 +63,8 @@ assert jan3rd['num_tweets'] - 1402 < 0.001
 assert jan3rd['pct_pos'] - 0.4265 < 0.001
 # assert df.iloc[12, :]
 
-y_binary = (daily_return > 0).astype('int8')
-y = daily_return
+y_binary = (df.Close > 0).astype('int8')
+y = df.Close.copy()
 
 
 # %%
@@ -104,23 +85,11 @@ def prep(dataf, scale, poly):
 
 
 # %%
-# ATTENTION! For experimenting!
-
-financial_only = False
-
-if financial_only:
-    c.print("[red] CAUTION: dropping all sentiment features, using financial only!")
-    df = df.drop(['sentiment', 'num_tweets', 'pct_neg', 'pct_pos', 'prev0', 'prev1', 'prev2'], axis=1)
-
-# df = df.drop(['prev0', 'prev1', 'prev2'], axis=1)
-
-
-
-SCORING = 'accuracy'
+SCORING = 'roc_auc'
 cv = KFold(10)
 
 svc2 = GridSearchCV(SVC(probability=True,),
-                    param_grid={'kernel': ['rbf', 'sigmoid'],
+                    param_grid={'kernel': ['rbf', 'poly' 'sigmoid'],
                                 'C': [0.01, 0.1, 0.5, 1, 5, 10]
                                 },
                     n_jobs=-1,
@@ -140,7 +109,7 @@ results = {
 
 
 for combo in combos:
-    X = prep(df, *combo)
+    X = prep(df.drop('Close', axis=1), *combo)
     c.print(f"[bold underline]Fitting combo {combo}")
 
     # LR
